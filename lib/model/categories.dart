9a161c62b36365b64a91050b798c6aab7bffda6b
mohammadAlmsalmeh <98343087+mohammadAlmsalmeh@users.noreply.github.com>
2022-05-24 17:03:44 +0300

    Update categories.dart

--- lib/model/categories.dart
+++ lib/model/categories.dart
@@ -11,69 +11,10 @@ import 'package:tnzelat/view/home.dart';
 
 class Categories{
 Future<List<dynamic>> getCategory(bool go) async{
-  String url ="https://www.t.me/s/testmytnzelat";
-  var result = await get(Uri.parse(url));
-  var data = result.body;
-  AppVar.contentPage = data;
-  await getCards(data);
-  const start = "{%CATEGORY%}";
-  const end = "{%CATEGORYEND%}";
-  final startIndex = data.indexOf(start);
-  final endIndex = data.indexOf(end, startIndex + start.length);
-  var theData = data.substring(startIndex + start.length, endIndex);
-  theData = theData.replaceAll("&quot;",'"');
-  var json = jsonDecode(theData);
-  var list =["All"];
-  if(json["status"]=="success"){
-    for(var j in json["data"]){
-      list.add(j);
-    }
-    AppVar.allCategory = list;
-    if(go){
-      Future.delayed(Duration(seconds: 2)).then((value) {
-        Get.off(Home());
-      });
-    }
-
-  }
-  else {
-    List<String> x = ["check your internet connection"];
-    AppVar.allCategory = x;
-  }
-  AppVar.isLoading = false;
-  return [];
 }
 
 Future<bool> getCards(String data) async{
-  //String url ="https://www.t.me/s/testmytnzelat";
-  //var result = await get(Uri.parse(url));
-  //var data = result.body;
-  const start = "{%CARDS%}";
-  const end = "{%CARDSEND%}";
-  final startIndex = data.indexOf(start);
-  final endIndex = data.indexOf(end, startIndex + start.length);
-  var theData = data.substring(startIndex + start.length, endIndex);
-  theData = theData.replaceAll("&quot;",'"');
-  var json = jsonDecode(theData);
-  if(json["status"]=="success"){
-    List<ShoppingModel> list =[];
-    for(var j in json["data"]){
-      var startImg =j["image"]+"BEGIN";
-      var endImg=j["image"]+"END";
-      final startImgIndex = data.indexOf(startImg);
-      var endImgIndex = data.indexOf(endImg);
-      var urlIn = data.substring(startImgIndex, endImgIndex);
-      var imgurl = urlIn.substring(urlIn.indexOf("background-image:url('")+"background-image:url('".length,
-      urlIn.indexOf(".jpg')")+".jpg".length);
-      ShoppingModel sh = ShoppingModel(j["name"],j["category"],j["price"],imgurl,j["detail"],j["address"]);
-      list.add(sh);
-    }
-    AppVar.AllCards = list;
-  }
-  else{
-
-  }
-  return true;
+  
 }
 
 }
